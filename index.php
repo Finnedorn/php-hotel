273d0e9d193e996e6180cd0c2000892c073bb450
Finnedorn <daniele.giuntoli@outlook.com>
2023-11-29 17:33:48 +0100

    working on select function

--- index.php
+++ index.php
@@ -1,41 +1,70 @@
 <?php 
 include __DIR__. '/partials/Header.php';
+
+if (isset($_GET['parking'])) {
+    $parking = $_GET['parking'];
+    if ($parking !== 'all') {
+        foreach($hotels as $hotel_detail){
+            if($hotel_detail['parking'] == (bool) $parking) {
+                $temporary_array[] = $hotel_detail;
+            }
+        }
+        $hotels = $temporary_array;
+    }
+}
+
+if (isset($_GET['vote']) && $_GET['vote'] !== 'all') {
+    $vote = intval($_GET['vote']);
+    foreach($hotels as $hotel_detail){
+        if($hotel_detail['vote'] >= $vote) {
+            $temporary_array[] = $hotel_detail;
+        }
+    }
+    $hotels = $temporary_array;
+}
+
 ?>
 
 <main class="container mb-3">
     <div class="card p-4 my-5">
-        <table class="table">
-            <thead>
-                <tr class="text-center">
-                    <th scope="col">Nome</th>
-                    <th scope="col">Descrizione</th>
-                    <th scope="col">Parcheggio</th>
-                    <th scope="col">Voto</th>
-                    <th scope="col">Distanza dal centro</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach($hotels as $hotel_detail) { ?>
+        <?php if(count($hotels) > 0) { ?>
+            <table class="table">
+                <thead>
                     <tr class="text-center">
-                        <?php foreach($hotel_detail as $key => $value) { ?>
-                            <?php if($key == 'parking' && $value === true) { ?>
-                                <td class="text-success">
-                                    <i class="fa-solid fa-check"></i>
-                                </td>
-                            <?php } elseif($value == false) { ?>
-                                <td class="text-danger">
-                                    <i class="fa-solid fa-x"></i>
-                                </td>
-                            <?php } else { ?>
-                            <td>
-                                <?php echo $value ?>
-                            </td>
-                            <?php } ?>
-                        <?php } ?>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Descrizione</th>
+                        <th scope="col">Parcheggio</th>
+                        <th scope="col">Voto</th>
+                        <th scope="col">Distanza dal centro</th>
                     </tr>
-                <?php } ?>
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    <?php foreach($hotels as $hotel_detail) { ?>
+                        <tr class="text-center">
+                            <?php foreach($hotel_detail as $key => $value) { ?>
+                                <?php if($key == 'parking' && $value === true) { ?>
+                                    <td class="text-success">
+                                        <i class="fa-solid fa-check"></i>
+                                    </td>
+                                <?php } elseif($value == false) { ?>
+                                    <td class="text-danger">
+                                        <i class="fa-solid fa-x"></i>
+                                    </td>
+                                <?php } else { ?>
+                                <td>
+                                    <?php echo $value ?>
+                                </td>
+                                <?php } ?>
+                            <?php } ?>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        <?php } else { ?>
+            <div class="m-auto">
+                Non ci sono risultati per questa ricerca!
+            </div>
+        <?php } ?>
     </div>
 </main>
 
--- partials/Header.php
+++ partials/Header.php
@@ -12,5 +12,27 @@ include __DIR__. '/../model/data.php';
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" integrity="sha512-Avb2QiuDEEvB4bZJYdft2mNjVShBftLdPG8FJ0V7irTLQ8Uo0qcPxh4Plq7G5tGm0rU+1SPhVotteLpBERwTkw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
-    <header class="container">
+    <header class="container my-3">
+        <div class="py-3 px-4">
+            <form action="index.php" method="GET">
+                <span>
+                    parcheggio
+                </span>
+                <select name="parking">
+                    <option value="all">-</option>
+                    <option value="1">Si</option>
+                    <option value="0">No</option>
+                </select>
+                <span>voto</span>
+                <select name="vote">
+                    <option value="all">-</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>
+                <button type="submit" class="btn btn-success">Cerca</button>
+            </form>
+        </div>
     </header>
