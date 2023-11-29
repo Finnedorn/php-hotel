c4f24a6858859e5e79868a72dbfc04516e1368cb
Finnedorn <daniele.giuntoli@outlook.com>
2023-11-29 16:15:31 +0100

    added table with data hotel details

--- index.php
+++ index.php
@@ -3,8 +3,39 @@ include __DIR__. '/partials/Header.php';
 ?>
 
 <main class="container mb-3">
-    <div class="row py-4">
-        
+    <div class="card p-4 my-5">
+        <table class="table">
+            <thead>
+                <tr class="text-center">
+                    <th scope="col">Nome</th>
+                    <th scope="col">Descrizione</th>
+                    <th scope="col">Parcheggio</th>
+                    <th scope="col">Voto</th>
+                    <th scope="col">Distanza dal centro</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($hotels as $hotel_detail) { ?>
+                    <tr class="text-center">
+                        <?php foreach($hotel_detail as $key => $value) { ?>
+                            <?php if($key == 'parking' && $value === true) { ?>
+                                <td class="text-success">
+                                    <i class="fa-solid fa-check"></i>
+                                </td>
+                            <?php } elseif($value == false) { ?>
+                                <td class="text-danger">
+                                    <i class="fa-solid fa-x"></i>
+                                </td>
+                            <?php } else { ?>
+                            <td>
+                                <?php echo $value ?>
+                            </td>
+                            <?php } ?>
+                        <?php } ?>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
     </div>
 </main>
 
